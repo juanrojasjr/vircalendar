41b688963573c41ff9d1e04710207b595f88faa7
Juan D. Rojas <juandarojas01@gmail.com>
2023-05-22 04:31:05 -0500

    Se cambia las rutas de archivos y se agrega la funcion de cambiar contraseña

--- js/login.js
+++ js/login.js
@@ -6,18 +6,18 @@ export function loginInit() {
       pass = $("#password").val();
     $.ajax({
       method: "POST",
-      url: "core/login.php",
+      url: "core/user/login.php",
       data: { nickname: nickname, pass: pass },
     }).done(function (msg) {
       const errors = JSON.parse(msg);
       const errContainer = $(".errorContainer");
-
       if (errors["error"]) {
         let html = `<div class="alert alert-${errors["error"] ? "danger" : "success"}" role="alert">${errors["msg"]}</div>`;
         errContainer.show();
         $(html).appendTo(errContainer);
       } else {
         sessionStorage.setItem("uid", errors["uid"]);
+        sessionStorage.setItem("firstname", errors["name"]);
         setTimeout(function () {
           window.location.href = "home";
         }, 1000);
@@ -31,16 +31,52 @@ export function loginInit() {
     const email = $("#email-forgot").val();
     $.ajax({
       method: "POST",
-      url: "core/forgot_password.php",
+      url: "core/user/forgot_password.php",
       data: { email: email },
     }).done(function (msg) {
       console.log(msg);
     });
   });
 
+  /* Cambiar contraseña */
+  $('#changePassModal button[type="submit"]').click(function (e) {
+    e.preventDefault();
+    $('#changePassModal input').removeClass('is-invalid');
+    $('#oldPassMsg').addClass('d-none')
+    let uid = sessionStorage.getItem('uid'),
+      oldPass = $("#oldPass").val(),
+      newPass = $("#newPass").val(),
+      repNewPass = $("#repNewPass").val();
+    if (newPass === repNewPass) {
+      $.ajax({
+        method: "POST",
+        url: "core/user/change_password.php",
+        data: { uid, oldPass, newPass },
+      }).done(function (msg) {
+        const result = JSON.parse(msg);
+        if (result['changed']) {
+          $('#changePassModal').modal('hide');
+          Swal.fire({
+            icon: "success",
+            title: "¡Listo!",
+            text: "La contraseña se ha actualizado.",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        } else {
+          $("#oldPass").addClass('is-invalid');
+          $('#oldPassMsg').removeClass('d-none');
+        }
+      });
+    } else {
+      $("#newPass").addClass('is-invalid');
+      $("#repNewPass").addClass('is-invalid');
+    }
+  });
+
   $('#closeSession').click(function (e) {
     sessionStorage.clear();
-    window.location.href = 'core/close.php';
+    window.location.href = 'core/user/close.php';
   })
 
 }
