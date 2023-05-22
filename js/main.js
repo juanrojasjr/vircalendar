922570ed9196a7c13a38002ef0fca718f4d02714
Juan D. Rojas <juandarojas01@gmail.com>
2023-05-22 04:31:33 -0500

    Se agrega el saludo de bienvenida para el usuario en el home

--- js/main.js
+++ js/main.js
@@ -2,40 +2,24 @@ import { fullcalendar } from './fullcalendar.js';
 import { eventInit } from './events.js';
 import { loginInit } from './login.js';
 
-const calendarEl = document.getElementById("calendar");
-
 document.addEventListener("DOMContentLoaded", () => {
+  const calendarEl = document.getElementById("calendar");
+
   eventInit();
   loginInit();
+
   if (calendarEl != null) {
     fullcalendar(calendarEl);
   }
+
+  if (sessionStorage.getItem('firstname')) {
+    document.querySelector('.firstname').innerHTML = 'Hola, ' + sessionStorage.getItem('firstname');
+  }
+
+  /* Elimina el calendario si no detecta un inicio de sesión */
+  if (calendarEl && !sessionStorage.getItem('uid')) {
+    $(calendarEl).remove();
+    $('.noLogin').removeClass('d-none');
+    $('#userMnu').addClass('d-none');
+  }
 });
-
-/* Elimina el calendario si no detecta un inicio de sesión */
-if (calendarEl && !sessionStorage.getItem('uid')) {
-  $(calendarEl).remove();
-  $('.noLogin').removeClass('d-none');
-}
-
-/* Reestablecer contraseña olvidada */
-// $('#forgot-password button[type="submit"]').click(function (e) {
-//   e.preventDefault();
-//   let username = $("#username").val();
-//   const alertD = $(".alert.alert-danger"),
-//     alertS = $(".alert.alert-success");
-//     alertD.addClass("d-none");
-//     alertS.addClass("d-none");
-//   $.ajax({
-//     method: "POST",
-//     url: "inc/forgot_password.php",
-//     data: { username: username },
-//   }).done(function (msg) {
-//     console.log(msg);
-//     if (msg == 0) {
-//       alertD.removeClass("d-none");
-//     } else {
-//       alertS.removeClass("d-none");
-//     }
-//   });
-// });
